39bd8775d5bdde4d7c2ac49fe1059f2344804e90
Camilobarre <barrecami@gmail.com>
2024-04-18 13:19:18 -0500

    Se termina actividad de operadores lógicos

--- src/js/actividades.js
+++ src/js/actividades.js
@@ -1,119 +1,151 @@
 console.groupCollapsed("Ejercicio 1");
 // Ejercicio 1
+console.log(35 > 25); //True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 2");
 // Ejercicio 2
+console.log(45 < 30); //False
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 3");
 // Ejercicio 3
+console.log(50 == "50"); //True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 4");
 // Ejercicio 4
+console.log(40 === 40); //True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 5");
 // Ejercicio 5
+console.log("apple" != "orange"); //True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 6");
 // Ejercicio 6
+console.log("apple" !== "Apple"); //True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 7");
 // Ejercicio 7
+let variableSinDefinir = undefined
+console.log(variableSinDefinir); //Undefined
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 8");
 // Ejercicio 8
+console.log(typeof variableSinDefinir); //Undefined
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 9");
 // Ejercicio 9
+let variableNula = null
+console.log(variableNula == variableSinDefinir); //True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 10");
 // Ejercicio 10
+console.log(variableNula === variableSinDefinir); //False
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 11");
 // Ejercicio 11
+console.log("2.75" > 2.5); //True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 12");
 // Ejercicio 12
+console.log("2.75" > 2.75); //False
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 13");
 // Ejercicio 13
+console.log("2.75" >= 2.75); //True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 14");
 // Ejercicio 14
+console.log("2.75" <= 2.75); //True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 15");
 // Ejercicio 15
+console.log("2.75" > 2.8); //False
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 16");
 // Ejercicio 16
+console.log("2.75" < 2.7); //False
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 17");
 // Ejercicio 17
+console.log("John" != "john"); //True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 18");
 // Ejercicio 18
+console.log("Jhon" != "Jon"); //True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 19");
 // Ejercicio 19
+console.log(30 !== "30");//True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 20");
 // Ejercicio 20
+console.log(25 >> 2); //6
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 21");
 // Ejercicio 21
+console.log(8 << 3); //64
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 22");
 // Ejercicio 22
+console.log(15 == 15); //True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 23");
 // Ejercicio 23
+console.log(20 != 30); //True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 24");
 // Ejercicio 24
+console.log("15" > 30); //False
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 25");
 // Ejercicio 25
+console.log(20 < 40); //True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 26");
 // Ejercicio 26
+console.log(100 >= "99"); //True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 27");
 // Ejercicio 27
+console.log(500 === "500"); //False
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 28");
 // Ejercicio 28
+console.log(750 === "750"); //False
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 29");
 // Ejercicio 29
+console.log(5 !== "5"); //True
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 30");
 // Ejercicio 30
+console.log(3 > 5 && 4 < 6); //False
 console.groupEnd();
