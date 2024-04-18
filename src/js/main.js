6eec524c83e93c8ff3b39b45abd43d3ce92fd0ae
Camilobarre <barrecami@gmail.com>
2024-04-18 11:08:16 -0500

    Se empiezan los ejercicios con operadores matematicos

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,44 @@
-alert("Hola Mundo")
+//Tipos de datos en JavaScript
+
+console.groupCollapsed('Tipos de datos');
+let variable1 = 156
+let variable2 = 854
+let variable3 = "hola mundo"
+let variable4 = true
+let variable5 = undefined
+let variable6 = null
+let variable7 = ["hola", "como", "estas", "en", "este", "dia"]
+let variable8 = {
+    "hola": "hola",
+    "hola": "hola",
+    "como": "como"
+}
+let variable9 = function name() { }
+
+console.log("Variable 1:", typeof variable1)
+console.log("Variable 2:", typeof variable2)
+console.log("Variable 3:", typeof variable3)
+console.log("Variable 4:", typeof variable4)
+console.log("Variable 5:", typeof variable5)
+console.log("Variable 6:", typeof variable6)
+console.log("Variable 7:", typeof variable7)
+console.log("Variable 8:", typeof variable8)
+console.log("Variable 9:", typeof variable9)
+console.groupEnd();
+
+//Operadores aritméticos
+
+console.groupCollapsed("Operadores Aritméticos");
+let numero1=84
+let numero2=15
+
+let suma=numero1+numero2
+let resta=numero1-numero2
+let multiplicacion=numero1*numero2
+let division=numero1/numero2
+
+console.log("Suma:", suma)
+console.log("Resta:", resta)
+console.log("Multiplicación:", multiplicacion)
+console.log("División:", division)
+console.groupEnd();
